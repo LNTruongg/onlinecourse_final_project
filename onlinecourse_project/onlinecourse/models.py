5a57afb1413b6f3b173a053d5280a013c0fa6d1f
Nhật Trường <lenhattruongbt71@gmail.com>
2026-05-25 11:47:13 +0700

    Update models.py

--- onlinecourse_project/onlinecourse/models.py
+++ onlinecourse_project/onlinecourse/models.py
@@ -17,6 +17,20 @@ class Lesson(models.Model):
         return self.title
 
 
+class Instructor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Learner(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
