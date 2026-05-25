9b23a33014f1378cb5373af02ffc5065ab0719f0
Nhật Trường <lenhattruongbt71@gmail.com>
2026-05-25 12:01:01 +0700

    Update urls.py

--- onlinecourse_project/onlinecourse/urls.py
+++ onlinecourse_project/onlinecourse/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('course/<int:course_id>/', views.course_detail, name='course_detail'),
-    path('submit/<int:course_id>/', views.submit, name='submit'),
-    path('result/<int:course_id>/', views.show_exam_result, name='show_exam_result'),
+    path('course/<int:course_id>/submit/', views.submit, name='submit'),
+
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
 ]
