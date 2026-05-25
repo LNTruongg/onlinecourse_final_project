626a60eea45b959bf58c5c6533ab66f0a41b66dc
Nhật Trường <lenhattruongbt71@gmail.com>
2026-05-25 11:36:37 +0700

    Update models.py

--- onlinecourse_project/onlinecourse/models.py
+++ onlinecourse_project/onlinecourse/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Course(models.Model):
     name = models.CharField(max_length=100)
 
@@ -37,3 +38,6 @@ class Submission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.course.name}"
